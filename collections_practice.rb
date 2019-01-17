770d4e6ab4348b6b28639287b3860407ecda6792
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-17 15:20:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,8 +4,8 @@ def sort_array_asc(integers)
   end
 end
 
-def sort_array_asc(integers)
-  integers.sort do |a, b|
-    b <=> a
-  end
-end
+# def sort_array_asc(integers)
+#   integers.sort do |a, b|
+#     b <=> a
+#   end
+# end
