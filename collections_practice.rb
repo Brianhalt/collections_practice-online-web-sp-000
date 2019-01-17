c5e8d0863271ee0ded95dc59d5a06e3010934677
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-17 15:28:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,7 @@ def sort_array_desc(integers)
 end
 
 def sort_array_char_count(strings)
-  strings.sort do {|a,b| a.lenth <=> b.lenth}
+  strings.sort do |a,b| 
+    a.lenth <=> b.lenth
   end
 end
